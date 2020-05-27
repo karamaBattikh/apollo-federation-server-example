29cda800a54e6ffcf64afaeda90042b47885a6f7
karamaBattikh <battikh.karama@gmail.com>
2020-05-27 11:53:55 +0100

    init project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server ready at http://localhost:${port}`);
+});
